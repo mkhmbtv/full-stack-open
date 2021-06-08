df1641ecc9d9bb23eb1e03f3898fd28c1a5657c5
your name here <mmakhambetov@yandex.ru>
2021-06-08 17:59:37 +0400

    Complete 2.1 - 2.3

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const Header = ({ name }) => (
+  <h1>{name}</h1>
+);
+
+const Content = (props) => (
+  <p>{props.name} {props.exercises}</p>
+);
+
+const Total = (props) => {
+  const total = props.parts.reduce((sum, part) => {
+    return sum += part.exercises;
+  }, 0);
+
+  return (
+    <p><b>total of {total} exercises </b></p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      {course.parts.map(part =>
+        <Content key={part.id} name={part.name} exercises={part.exercises} />
+      )}
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
+
