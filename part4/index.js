6b61fd4fbdd52c3a93a638bd99b05f9f8187c3ac
your name here <mmakhambetov@yandex.ru>
2021-06-20 00:51:05 +0400

    Complete 4.1

---
+++ part4/index.js
@@ -0,0 +1,61 @@
+require('dotenv').config()
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const mongoUrl = process.env.MONGODB_URI
+  
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+    
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+app.post('/api/blogs', (request, response) => {
+    const body = request.body
+    
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    })
+    
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
