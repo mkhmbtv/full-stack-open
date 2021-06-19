99b862bfc8e0faed944067abf6f25b4bcb1a3a7f
your name here <mmakhambetov@yandex.ru>
2021-06-20 02:19:18 +0400

    Fix blogs controller

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,7 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/api/blogs', (request, response) => {
+blogsRouter.get('/', (request, response) => {
     Blog
         .find({})
         .then(blogs => {
@@ -9,7 +9,7 @@ blogsRouter.get('/api/blogs', (request, response) => {
         })
 })
 
-blogsRouter.post('/api/blogs', (request, response) => {
+blogsRouter.post('/', (request, response) => {
     const body = request.body
 
     const blog = new Blog({
