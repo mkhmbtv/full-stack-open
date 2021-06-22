38b8ed11570b5b777ccc5521e193e43c8f9c90df
your name here <mmakhambetov@yandex.ru>
2021-06-22 20:45:47 +0400

    Complete 4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -38,6 +38,27 @@ test('blog has id property', async () => {
   expect(blogs[0].id).toBeDefined()
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(blog => blog.title)
+  expect(titles).toContain('Type wars')
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
