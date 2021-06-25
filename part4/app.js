03466ef7cf717597732b3f099a6dfec80c40ffe5
your name here <mmakhambetov@yandex.ru>
2021-06-25 17:31:42 +0400

    Complete 4.20

--- part4/app.js
+++ part4/app.js
@@ -28,6 +28,7 @@ morgan.token('body', (req) => {
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
