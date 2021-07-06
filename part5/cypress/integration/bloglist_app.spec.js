5fd51c28bb1d9fe892a343b01e10fc75b308bdf2
your name here <mmakhambetov@yandex.ru>
2021-07-06 20:44:06 +0400

    Complete 5.18

--- part5/cypress/integration/bloglist_app.spec.js
+++ part5/cypress/integration/bloglist_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'testing'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +15,27 @@ describe('Blog app', function() {
     cy.get('#username').should('be.visible')
     cy.get('#password').should('be.visible')
     cy.get('#login-button').contains('login')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('root')
+      cy.get('#password').type('testing')
+      cy.get('#login-button').click()
+
+      cy.contains('Superuser logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('root')
+      cy.get('#password').type('password')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .contains('invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.get('html').should('not.contain', 'Superuser logged in')
+    })
   })
 })
