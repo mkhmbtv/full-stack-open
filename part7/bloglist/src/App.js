e95488e087683547b549add024829f3fa7dbee48
your name here <mmakhambetov@yandex.ru>
2021-07-21 23:10:28 +0400

    Complete 7.13

--- part7/bloglist/src/App.js
+++ part7/bloglist/src/App.js
@@ -3,10 +3,18 @@ import BlogForm from './components/BlogForm'
 import BlogList from './components/BlogList'
 import LoginForm from './components/LoginForm'
 import Notification from './components/Notification'
+import Users from './components/Users'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { initializeBlogs } from './reducers/blogReducer'
+import { initializeUsers } from './reducers/usersReducer'
 import { logout } from './reducers/userReducer'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom'
 
 
 const App = () => {
@@ -15,6 +23,7 @@ const App = () => {
 
   useEffect(() => {
     dispatch(initializeBlogs())
+    dispatch(initializeUsers())
   }, [dispatch])
 
   const handleLogout = () => {
@@ -31,14 +40,23 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h2>blogs</h2>
-      <Notification />
-      {user.name} logged in
-      <button type="submit" onClick={handleLogout}>logout</button>
-      <BlogForm />
-      <BlogList />
-    </div>
+    <Router>
+      <div>
+        <h2>blogs</h2>
+        <Notification />
+        <p>{user.name} logged in</p>
+        <button type="submit" onClick={handleLogout}>logout</button>
+        <Switch>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <BlogForm />
+            <BlogList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
--- part7/bloglist/src/store.js
+++ part7/bloglist/src/store.js
@@ -4,11 +4,13 @@ import thunk from 'redux-thunk'
 import blogReducer from './reducers/blogReducer'
 import notificationReducer from './reducers/notificationReducer'
 import userReducer from './reducers/userReducer'
+import usersReducer from './reducers/usersReducer'
 
 const reducer = combineReducers({
   blogs: blogReducer,
   notification: notificationReducer,
-  user: userReducer
+  user: userReducer,
+  users: usersReducer
 })
 
 export default createStore(reducer, composeWithDevTools(
